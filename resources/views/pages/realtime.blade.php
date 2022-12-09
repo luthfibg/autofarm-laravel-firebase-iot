30aa11125c10be01f114fdc7cbfd07a045645e1e
luthfibg <luthfinewzoneonline@gmail.com>
2022-12-09 17:32:10 +0700

    [INIT] firebase

---
+++ resources/views/pages/realtime.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.main')
+
+@section('content-title')
+    Realtime Data <span> <i>Refresh to show current data</i> </span>
+@endsection
+
+@section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-md-4">
+                    <h2>Status now: {{ $value }}</h2>
+                </div>
+                <div class="col-12 col-md-4"></div>
+                <div class="col-12 col-md-4">
+                    @switch($value)
+                        @case('0')
+                            <button class="btn btn-primary"> Set On </button>
+                            @break
+                        @case('1')
+                            <button class="btn btn-danger"> Set Off </button>
+                            @break
+                        @default
+                            
+                    @endswitch
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
