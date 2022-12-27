3b120e7998db5ee2bb2255e02bb12ba4ccb0f526
luthfibg <luthfinewzoneonline@gmail.com>
2022-12-27 12:44:31 +0700

    [UPD] deploy seeder user data to admin

--- resources/views/pages/userlist.blade.php
+++ resources/views/pages/userlist.blade.php
@@ -49,20 +49,24 @@
                                 </div>
                             </th>
                             <th scope="col" class="py-3 px-6">
+                                Username
+                            </th>
+                            <th scope="col" class="py-3 px-6">
                                 Name
                             </th>
                             <th scope="col" class="py-3 px-6">
-                                Position
+                                Email
                             </th>
                             <th scope="col" class="py-3 px-6">
-                                Status
+                                Password
                             </th>
                             <th scope="col" class="py-3 px-6">
                                 Action
-                            </th>
+                            </th>                        
                         </tr>
                     </thead>
                     <tbody>
+                        @forelse ($users as $key => $customer)
                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                             <td class="p-4 w-4">
                                 <div class="flex items-center">
@@ -73,126 +77,32 @@
                             <th scope="row" class="flex items-center py-4 px-6 text-gray-900 whitespace-nowrap dark:text-white">
                                 <img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese image">
                                 <div class="pl-3">
-                                    <div class="text-base font-semibold">Neil Sims</div>
-                                    <div class="font-normal text-gray-500">neil.sims@flowbite.com</div>
+                                    <div class="text-base font-semibold text-gray-500">{{ $customer['name'] }}</div>
+                                    <div class="font-normal text-gray-500">{{ $customer['email'] }}</div>
                                 </div>  
                             </th>
                             <td class="py-4 px-6">
-                                React Developer
+                                {{ $customer['uname'] }}
                             </td>
                             <td class="py-4 px-6">
                                 <div class="flex items-center">
-                                    <div class="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div> Online
+                                    {{ $customer['email'] }}
+                                    {{-- <div class="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div>  --}}
                                 </div>
+                            </td>
+                            <td class="py-4 px-6">
+                                {{ $customer['pass'] }}
+                                {{-- <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a> --}}
                             </td>
                             <td class="py-4 px-6">
                                 <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
                             </td>
                         </tr>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td class="p-4 w-4">
-                                <div class="flex items-center">
-                                    <input id="checkbox-table-search-2" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="checkbox-table-search-2" class="sr-only">checkbox</label>
-                                </div>
-                            </td>
-                            <th scope="row" class="flex items-center py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                <img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Jese image">
-                                <div class="pl-3">
-                                    <div class="text-base font-semibold">Bonnie Green</div>
-                                    <div class="font-normal text-gray-500">bonnie@flowbite.com</div>
-                                </div>
-                            </th>
-                            <td class="py-4 px-6">
-                                Designer
-                            </td>
-                            <td class="py-4 px-6">
-                                <div class="flex items-center">
-                                    <div class="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div> Online
-                                </div>
-                            </td>
-                            <td class="py-4 px-6">
-                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
-                            </td>
-                        </tr>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td class="p-4 w-4">
-                                <div class="flex items-center">
-                                    <input id="checkbox-table-search-2" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="checkbox-table-search-2" class="sr-only">checkbox</label>
-                                </div>
-                            </td>
-                            <th scope="row" class="flex items-center py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                <img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-2.jpg" alt="Jese image">
-                                <div class="pl-3">
-                                    <div class="text-base font-semibold">Jese Leos</div>
-                                    <div class="font-normal text-gray-500">jese@flowbite.com</div>
-                                </div>
-                            </th>
-                            <td class="py-4 px-6">
-                                Vue JS Developer
-                            </td>
-                            <td class="py-4 px-6">
-                                <div class="flex items-center">
-                                    <div class="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div> Online
-                                </div>
-                            </td>
-                            <td class="py-4 px-6">
-                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
-                            </td>
-                        </tr>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td class="p-4 w-4">
-                                <div class="flex items-center">
-                                    <input id="checkbox-table-search-2" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="checkbox-table-search-2" class="sr-only">checkbox</label>
-                                </div>
-                            </td>
-                            <th scope="row" class="flex items-center py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                <img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Jese image">
-                                <div class="pl-3">
-                                    <div class="text-base font-semibold">Thomas Lean</div>
-                                    <div class="font-normal text-gray-500">thomes@flowbite.com</div>
-                                </div>
-                            </th>
-                            <td class="py-4 px-6">
-                                UI/UX Engineer
-                            </td>
-                            <td class="py-4 px-6">
-                                <div class="flex items-center">
-                                    <div class="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div> Online
-                                </div>
-                            </td>
-                            <td class="py-4 px-6">
-                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
-                            </td>
-                        </tr>
-                        <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td class="p-4 w-4">
-                                <div class="flex items-center">
-                                    <input id="checkbox-table-search-3" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="checkbox-table-search-3" class="sr-only">checkbox</label>
-                                </div>
-                            </td>
-                            <th scope="row" class="flex items-center py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                <img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-4.jpg" alt="Jese image">
-                                <div class="pl-3">
-                                    <div class="text-base font-semibold">Leslie Livingston</div>
-                                    <div class="font-normal text-gray-500">leslie@flowbite.com</div>
-                                </div>
-                            </th>
-                            <td class="py-4 px-6">
-                                SEO Specialist
-                            </td>
-                            <td class="py-4 px-6">
-                                <div class="flex items-center">
-                                    <div class="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div> Offline
-                                </div>
-                            </td>
-                            <td class="py-4 px-6">
-                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
-                            </td>
-                        </tr>
+                        @empty
+                        <td class="py-4 px-6" colspan="7">
+                            <span>No Record Found</span>
+                        </td>
+                    @endforelse
                     </tbody>
                 </table>
             </div>
